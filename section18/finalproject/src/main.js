79a6df836eeaa6c40d328073b806207082afc949
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2019-03-27 01:02:38 +0100

    Remove package-lock.json

---
+++ section18/finalproject/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App.vue';
+import { routes } from './routes';
+import store from './store/store';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-4c9ae.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
+
+new Vue({
+  el: '#app',
+  router: router,
+  store: store,
+  render: h => h(App)
+})
